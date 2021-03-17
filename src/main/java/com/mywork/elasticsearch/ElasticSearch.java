a5c751c33c4a6b9828607e592a4e9d1d73358859
anildogiparthi <anildogiparthi@gmail.com>
2021-03-17 22:56:16 +0530

    Changes

---
+++ src/main/java/com/mywork/elasticsearch/ElasticSearch.java
@@ -0,0 +1,19 @@
+package com.mywork.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.mywork.elasticsearch")
+public class ElasticSearch
+{ 
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ElasticSearch.class, args);
+    }
+}
